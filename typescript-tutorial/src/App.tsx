50b13010f553718275baa0cb069c2c18cadc17ae
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-10-15 20:05:03 +0200

    typescript

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+
+let age: number;
+
+// "https://www.youtube.com/watch?v=FJDVKeh7RJI&t=147s&ab_channel=freeCodeCamp.org" minute 6:02
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
