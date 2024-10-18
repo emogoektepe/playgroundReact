b05861c8481930ec533605951db804df051ebe58
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-10-18 17:44:37 +0200

    todo done

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,13 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
-// "https://www.youtube.com/watch?v=FJDVKeh7RJI&t=147s&ab_channel=freeCodeCamp.org" minute 6:02
+// "https://www.youtube.com/watch?v=FJDVKeh7RJI&t=147s&ab_channel=freeCodeCamp.org" minute 18:15
 
-function App() {
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+
   return (
-    <div className="App">
-      Hello World
+    <div className="app">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setCompletedTodos} />
     </div>
   );
-}
+};
 
 export default App;
