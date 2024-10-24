9176813ed9af97050e9f3cc82b14b00fd795cae3
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-10-24 17:08:30 +0200

    spread operator

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
-import { Todo } from './model';
 import TodoList from './components/TodoList';
+import { Todo } from './model';
 
 // "https://www.youtube.com/watch?v=FJDVKeh7RJI&t=147s&ab_channel=freeCodeCamp.org" minute 18:15
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
     if (todo) {
-      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      const newTodo = [...todos, { id: Date.now(), todo: todo, isDone: false }];
+      setTodos(newTodo);
       setTodo("");
+      localStorage.setItem('todos', JSON.stringify(newTodo));
     }
   };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
 
   return (
     <div className="app">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setCompletedTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
--- typescript-tutorial/src/components/TodoList.tsx
+++ typescript-tutorial/src/components/TodoList.tsx
@@ -6,16 +6,20 @@ import SingleTodo from './SingleTodo';
 interface Props {
     todos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-    completedTodos: Todo[];
-    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ({ todos, setTodos, completedTodos, setCompletedTodos }) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
     return (
         <div className="todos">
-            {todos.map(todo => (
-                <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-            ))}
+            {
+                todos.length > 0 ? (
+                    todos.map(todo => (
+                        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+                    ))
+                ) : (
+                    <div style={{ textAlign: "center", fontSize: "50px" }}>Nothing to do here!</div>
+                )
+            }
         </div>
     );
 };
